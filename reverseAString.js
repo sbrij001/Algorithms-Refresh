46d8cdf24535eaa14ae6b93a85392bfe0216bd0b
shivani brijmohan <shivani.brijmohan@gmail.com>
2020-12-03 11:39:44 -0500

    created reverseAString.js and implemented the code to reverse a string

---
+++ reverseAString.js
@@ -0,0 +1,19 @@
+// create a function that reverses a string 
+// "Hi my name is Shivani" should be 
+// "inavihS si eman ym iH"
+
+
+// Check input
+//
+function reverse(string){
+  if(string.length < 2 || typeof str != "string"){
+    return string
+  }
+  const reverse = [];
+  const strArray = string.split("");
+  // strArray = ["s","h","i","v","a","n","i"]
+  for( i = strArray.length-1; i >= 0; i-- ){
+    reverse.push(strArray[i]);
+  }
+  return reverse.toString();
+};
